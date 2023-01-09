e27208677c6005c653f74267f6816e82a6f3bbac
domitrixl <92016515+domitrixl@users.noreply.github.com>
2023-01-09 16:01:08 +0100

    Update page_messages.xaml.cs

--- Client/SocialMediaApp/SocialMediaApp/page_messages.xaml.cs
+++ Client/SocialMediaApp/SocialMediaApp/page_messages.xaml.cs
@@ -10,6 +10,7 @@ public partial class page_messages : ContentPage
 
     private async void btn_newmsg_ClickedAsync(object sender, EventArgs e)
     {
+    //On click user gets promted for a username then opens the chat with the user by pushing page_inchats onto the stack
         name = await DisplayPromptAsync("Who shall be messaged?", "Name of your friend:");
         await Navigation.PushAsync(new page_inchats());
     }
